fde7e23b107a95ab9364ca8c37e2907286b39e8b
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-02-20 18:51:47 -0500

    Modify the parameters in the methods for making the project more open.

--- src/file.cpp
+++ src/file.cpp
@@ -1,27 +1,26 @@
 #include <stdio.h>
-
+#define NUM_DATA 1
 FILE *data;
 
-inline void write(animal &a){
+inline void write(animal &a,int pos){
     data = fopen("var/data.bin", "wb");
     if (data != NULL) { //SUCCESS OPERATION?
-        cout << "write pos:  " << sizeof(animal)*get_hash(a.name) << endl;
-        fseek(data,sizeof(animal)*get_hash(a.name),SEEK_SET);
-        fwrite(&a, sizeof(animal), 1, data);
+        cout << "write pos:  " << sizeof(animal)*pos<< endl;
+        fseek(data,sizeof(animal)*pos,SEEK_SET);
+        fwrite(&a, sizeof(animal), NUM_DATA, data);
         fclose(data);
-
     }else{
         cout << "Error opening the data." << endl;
     }
 }
 
-inline void read(string name){
+inline void read(string name, int pos){
     data = fopen("var/data.bin", "rb");
     animal a;
     if (data != NULL) { //SUCCESS OPERATION?
-        cout << "read pos:  " << sizeof(animal)*get_hash(name) << endl;
-        fseek(data,sizeof(animal)*get_hash(name),SEEK_SET);
-        fread(&a,sizeof(animal),1,data);
+        cout << "read pos:  " << sizeof(animal)*pos << endl;
+        fseek(data,sizeof(animal)*pos,SEEK_SET);
+        fread(&a,sizeof(animal),NUM_DATA,data);
         cout << "Name: "<< a.name << endl;
         fclose(data);
     }else{ 
