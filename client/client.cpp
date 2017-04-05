16841f8ac88217e92b7af337dbe4624a425b731c
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-05 09:48:42 -0500

    Send a custom-made message by sockets.

--- client/client.cpp
+++ client/client.cpp
@@ -4,6 +4,9 @@
 #include <arpa/inet.h>      /* for inet_addr */
 #include <stdio.h>
 #include <strings.h>
+#include <string>
+#include <vector>
+#include <iostream>
 #include <unistd.h>
 
 #define PORT 3535  /* This need to be  the same port  of the server part */
@@ -37,9 +40,23 @@ int main(){
     if(r == ERROR){
         perror("Woops, there was an error connecting...");
     }
+    cout << " \n\t\t Here I am going to show that the user can do. \n";
+    cout << "Option 1 (1)" << endl;
+    cout << "Option 2 (2)" << endl;
+    cout << "Option 3 (3)" << endl;
+    cout << "Option 4 (4)" << endl;
+    cout << "Option 5 (5)" << endl;
 
-    r = send(clientfd, "Hello world", TENBYTES, 0);
-    if (r != TENBYTES){
+    cout << "Write your name : ";
+
+    vector < char > buffer;
+    string name;
+    cin >> name;
+    for (int i= 0; i<name.size(); i++)
+        buffer.push_back(name[i]);
+
+    r = send(clientfd, buffer.data(), buffer.size(), 0);
+    if (r != buffer.size()){
         //Re send;
     }
     close(clientfd);
