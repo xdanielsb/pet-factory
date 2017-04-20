345ed748feef1b056370059b90cdcd7a22d8b84e
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-19 19:58:43 -0500

    Make robust the connections client-server.

--- client/client.cpp
+++ client/client.cpp
@@ -1,231 +1,79 @@
-#include <sys/types.h>          /* See NOTES */
+/* Libraries*/
+#include <bits/stdc++.h>
+#include <sys/types.h>
 #include <sys/socket.h>     /* for inet_addr */
 #include <netinet/in.h>     /* for inet_addr */
 #include <arpa/inet.h>      /* for inet_addr */
-#include <stdio.h>
-#include <strings.h>
-#include <string>
-#include <vector>
-#include <iostream>
 #include <unistd.h>
 
-
-#define PORT 9990  /* This need to be  the same port  of the server part */
-#define LOOPBACK "127.0.0.1"
+/* Constants */
+#define PORT 9990
+#define DIR "127.0.0.1"
 #define ERROR -1
-#define ONLY 0 //When there is just one  protocol that supports the socket
+#define ONLY 0              /* When there is just one  protocol that supports the socket */
+#define CTRLC 2
 
+/* Custom libraries */
 using namespace std;
-
 #include "animal.cpp"
-//#include "client_controller.cpp"
-#include <fstream>
-#include <sstream>
-#include <string>
 
-int clientfd;
+/*  Global variables */
+int clientfd;               /* File  descriptor client */
 struct sockaddr_in client;
+struct sigaction sigIntHandler; /*Handler*/
+bool flag = true;
 
-
-
-void insert_animal(){
-    int r; //Response -Success operation -- Error
-    //Define the necesary fields for inserting an animal
-    char name[32];
-    char type[32];
-    char breed[16];
-    char genre;
-    int32_t age;
-    int32_t height;
-    int32_t weight;
-
-    cout << "\n INSERT REGISTER \n";
-    cout << " User we are going to ask you for some data, please ingress the data : "<<endl;
-    cout << "\n\tName   = "; cin >> name;
-    cout << "\n\ttype   = "; cin >> type;
-    cout << "\n\tbreed  = "; cin >> breed;
-    cout << "\n\tGenre if it is Female write a {F} but if it is Male  write  a {M}\n";
-    cout << "\n\tgenre  = "; cin >> genre;
-    cout << "\n\tage    = "; cin >> age;
-    cout << "\n\theight = "; cin >> height;
-    cout << "\n\tweight = "; cin >> weight;
-
-
-    //Instance the animal
-    animal a1(name, type, age, breed,height, weight, genre);
-
-    //animal a1("Zeus", "Lion", 23, "Warrior", 12, 34, 'M');
-
-    r = send(clientfd, &a1, sizeof(a1), 0);
-    if (r != sizeof(a1)){
-        //Re send;
-        cout << "The message have not been sent completely. \n";
-    }else{
-        cout << "The animal has been sent. \n";
-    }
-    char  answer[100];
-    //Now receive the answer of the server about the operation
-    r = recv(clientfd, answer, 100, 0);
-    cout << endl << endl;
-    cout << "Server response: " << answer << endl;
-}
-
-
-void show_animal(){
-    int r; //Response -Success operation -- Error
-    char  answer[100];
-    int number_register;
-    animal a1;
-    cout << "\n SHOW REGISTER \n";
-    //Receive the number of clients
-
-    r = recv(clientfd, answer, 100, 0);
-
-    cout << " Total number of registers is: " << answer;
-    cout << " User write the number of the register to see, please" << endl;
-    cout << " Number of the register = " ;
-    cin >> number_register;
-
-    r = send(clientfd, &number_register, sizeof(number_register), 0);
-    //Receive the answer of the user  'animal'
-    r = recv(clientfd, &a1, sizeof(a1), 0);
-    cout << endl << "This is the animal" << endl;
-    cout << to_string(a1) << endl;
-
-    string cadena = "echo  \""+to_string(a1)+ "\" > hist.clinic ";
-    system(cadena.c_str());
-    system("gedit hist.clinic");
-
-    cout << "Do you wish to save the changes in the file (Y)es (N)ot\n";
-    char opt;
-    //Now we are going to do the update function
-    cin >> opt;
-    if(opt == 'Y' || opt == 'y'){
-        //Update data
-        ifstream infile("hist.clinic");
-        string line;
-        cout << "This is the animal that you want to save" << endl;
-        while (getline(infile, line)){
-            cout << line << endl;
-        }
-    }if(opt == 'N' || opt == 'n'){
-        //Do nothing
+void my_handler_signals(int signal){
+    printf("\nClient: Caught signal %d \n",signal);
+    if (signal == CTRLC){
+        printf("Client : Closing the socketfd \n");
+        close(clientfd);
+        exit(0);
     }
 }
-
-
-void delete_animal(){
-    int r; //Response -Success operation -- Error
-    //show_hash_table();
-    char  answer[100];
-    int number_register;
-    cout << "\n DELETE REGISTER \n";
-    cout << " Number of the register = " ;
-
-    cin >> number_register;
-    r = send(clientfd, &number_register, sizeof(number_register), 0);
-
-    //Now receive the answer of the server about the operation
-    r = recv(clientfd, answer, 100, 0);
-    cout << endl << endl;
-    cout << "Server response: " << answer << endl;
-}
-
-
-void show_by_name(){
-    int r; //Response -Success operation -- Error
-    vector < char > buffer;
-    string name;
-    animal a1;
-    cout << "\n SHOW ANIMALS BY NAME\n";
-
-    cout << " Name of the register = " ; cin >> name;
-
-    for (int i= 0; i<name.size(); i++)
-        buffer.push_back(name[i]);
-    //Send answer to the client
-    r = send(clientfd, buffer.data(), buffer.size(), 0);
-
-    //Receive the answer of the user  'animal'
-    r = recv(clientfd, &a1, sizeof(a1), 0);
-    //Show the animal
-    show_animal(a1);
-}
-
-
-void main_menu(){
-    cout << "\n\tHello User, Welcome to Pet Factory" << endl;
-    cout << "\n\t\t This are the options that we offer" << endl;
-    cout << "\t1. Insert a register " <<endl;
-    cout << "\t2. Show a register " <<endl;
-    cout << "\t3. Delete  a register " <<endl;
-    cout << "\t4. Search for a register " <<endl;
-    cout << "\t5. Salir " <<endl;
-    cout << "user-clinica> ";
-}
-
-void menu(int opt){
-    cout << "Option of the user: " << opt << endl;
-    if (opt == 1){
-        insert_animal();
-    }
-    else if (opt == 2){
-        show_animal();
-    }
-    else if(opt == 3){
-        delete_animal;
-    }else if(opt == 4){
-        show_by_name();
-    } else if( opt == 5){
-        cout << "\n BYE USER \n" << endl;
-        cout << "\n\t" << "SEE YOU LATER" << endl;
-    }
-}
-
 
 
 void create_client(){
-    int r;
+    int res, opcion;
+
+    /* Events client*/
+    sigIntHandler.sa_handler = my_handler_signals;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+    /* Create the socket  */
     clientfd = socket(AF_INET, SOCK_STREAM, ONLY);
-    if(clientfd == ERROR) {
+    if(clientfd == ERROR)
         perror("Woops, there was an error creating the socket...") ;
-    }
+
+    /*  Necesary data for the request */
     client.sin_family = AF_INET;
 	client.sin_port = htons(PORT);
-	client.sin_addr.s_addr = inet_addr ("127.0.0.1");
+	client.sin_addr.s_addr = inet_addr (DIR);
 
-    /* THis is use as a padding to make the structure of the same
-        size as sockaddr */
-    bzero(client.sin_zero, 8); /*Advise for standar posix */
+    /*Advise for standar posix */
+    bzero(client.sin_zero, 8);
 
-    socklen_t size;
-    size = sizeof(client);
-    r = connect(clientfd, (struct sockaddr*)&client, size);
-    if(r == ERROR){
+    /* Connect to the server */
+    res = connect(clientfd, (struct sockaddr*)&client, sizeof(client));
+    if(res == ERROR)
         perror("Woops, there was an error connecting...");
-    }
 
-    int intr =1;
-    bool flag = true;
+
     while(flag){
-        //Display main menu
-        main_menu();
+        /* Read the option */
+        scanf("%d", &opcion);
 
-        //Option that the user choose
-        int opcion;
-        cin >> opcion;
+        /* Send the request to the server */
+        res = send(clientfd, &opcion, sizeof(opcion), 0);
 
-        r = send(clientfd, &opcion, sizeof(opcion), 0);
-        if (r != sizeof(opcion)){
-            //Re send;
-            cout << "CLIENT: The option have not been sent completely. \n";
-        }
+        /* Check if the all message was sent */
+        if (res != sizeof(opcion))
+            printf("CLIENT: The option have not been sent completely. \n");
 
-        //Now after request is sent called another menu
-        menu(opcion);
-        if(opcion == 5){
-            flag = false;
-        }
+        /* Condition for exit */
+        if(opcion == 5) flag = false;
     }
     close(clientfd);
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <stdio.h>
-#include <sys/types.h>  /* See NOTES */
+/* Libraries*/
+#include <bits/stdc++.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>   //htons
 #include <netinet/in.h>  //structs socketaddr_in
-#include <strings.h>
-#include <string>
 #include <unistd.h>
-#include <map>
+
+
+/* Constants */
 #define PORT 9990
 #define BACKLOG 8
 #define ERROR -1
-#define NUM_CLIENTS 3
+#define NUM_CLIENTS 5
+#define CTRLC 2
 
 using namespace std;
+
+/* Custom Libraries */
 
 //-----No dependencies
 #include "src/animal.cpp"
@@ -23,128 +26,146 @@ using namespace std;
 //-----Dependencies
 #include "src/hash_table.cpp"
 #include "src/animal_controller.cpp"
-#include "src/application_controller.cpp"    //Declare animal
-#include "src/test.cpp"                      //Test of application functionalities
-#include "src/random.cpp"                    //Use hash_table for show
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "src/application_controller.cpp"
+#include "src/random.cpp"
 
 /*  Global variables */
-map < int , string > clients_map;
+map < int , string > clients_map;   /* (clientfd, ip_client) */
+bool flag = true;                   /* Continue the program? */
+int res;                            /* response of the operations */
+struct sigaction sigIntHandler;     /* Handler */
+int server_code;                    /* Server code */
 
-void check_user_option_main_menu(int request, int socketfd ){
-    int r;
-    string cadena = "";
+/*
+ * HANDLER SIGNALS
+ */
 
-    string user = to_string(socketfd);
+void my_handler_signals(int signal){
+    printf("\n Server : Caught signal %d \n",signal);
+    if (signal == CTRLC){
+        printf("Server : Closing the server \n");
+        close(server_code);
+        printf("Close the connection with the clients\n");
+        for (map<int,string>::iterator it=clients_map.begin(); it!=clients_map.end(); ++it){
+            printf("close connection %d\n", it->first);
+            close(it->first);
+        }
+        exit(0);
 
-    //Insert a pet
-    if(request == 1){
-        cadena += " INSERT ";
-        options_main_menu(1, socketfd);
     }
-    //Show a pet
-    else if(request == 2){
-        cadena += " SHOW ";
-        options_main_menu(2, socketfd);
-    }
-    //Delete a pet
-    else if(request == 3){
-        cadena += " DELETE ";
-        options_main_menu(3, socketfd);
-    }
-    //Search for a register
-    else if(request == 4){
-
-        string aux = options_main_menu(4, socketfd);
-        //Return the name to search
-        cadena +=" SEARCH "+ aux +" ";
-    }
-    else if(request == 5){
-        cadena +=" EXIT ";
-    }
-
-    cadena ="date \"+%H:%M:%S %d/%m/%y\" >>log.log && echo \" "+cadena+" " + clients_map[socketfd]+" \" >> log.log";
-    system(cadena.c_str());
-
-    //Exit 5 option not yet
 }
 
-void * function (void *ap){
-	int request;
-    bool flag = true;
-    double val= 0;
-	int r;
-    int socketfd =  *(int*)ap; //WOW --> not evident
+/*
+ * This function load or create the random data
+ */
+void load_data(bool from_scratch){
+    if(!from_scratch){
+        printf("\t Server: The data has  been read from disk\n");
+        data = read_file_hash(); //hash_table
+        long long total = 0;
+
+        /* Count registers */
+        for(int i=0; i< size_hash_table; i++)
+            total += data[i].size();
+
+        /* Pointer last register*/
+        LOC = total;
+        total --;
+        printf("\t Server: The number or registers that were readen from disk were: %lld\n",total);
+    }else{
+        create_random_data();
+        write_hash_table();
+    }
+}
+
+/*
+ * This function hold receive the request of the clients
+ */
+void * hold_client (void *ap){
+    int option;
+    int socketfd =  *(int*)ap;
+
 	while(flag){
-	    cout << "\n\n\t\tServer:Waiting for a request ... " << endl;
-        while(request <= 0 || request >=6){
-            r = recv(socketfd, &request, sizeof(request), 0);
-            cout << r;
+	    cout << "\tServer: Waiting for a request ... \n";
+        res = recv(socketfd, &option, sizeof(option), 0);
+
+        printf("Client %d: send the option: %d", socketfd, option);
+        /* This is in case of garbage close that connection */
+        if(option > 5 || option < 0) {
+            printf("\nClose client %d \n", socketfd);
+            close(socketfd);
+            break;
         }
-        cout << request <<endl;
-        if(request == 5){
-            cout << "Server BYE BYE";
-            flag = false;
-        }
-        check_user_option_main_menu(request, socketfd);
+
+        if(option == 5) flag = false;
 	}
 	close(socketfd);
 }
 
-
-
+/*
+ * This function is responsible for create the server
+ * and receive the  connections
+ */
 void create_server(){
-	int server_code = socket(AF_INET, SOCK_STREAM, 0 );
-	int size = sizeof(struct sockaddr);
+
+    /* Events Server signals*/
+    sigIntHandler.sa_handler = my_handler_signals;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
+
+    /* Variables for the server */
+	server_code = socket(AF_INET, SOCK_STREAM, 0 );
 	char buffer[16];
 	pthread_t hilo[3];
 
-	if(server_code == -1){
-		cerr << "SERVER :: Error creating the socket" << endl;
-	}
+    /* Variables for the client */
+	int client_code;
+	struct sockaddr_in  client;
+	size_t size_cli;
+    int cont_client = 0;
+
+    /*Check the socket initalization */
+	if(server_code == ERROR) perror("SERVER :: Error creating the socket\n");
 
 
-	//Now we are going to configure the socket
+	/* Socket configuration */
 	struct sockaddr_in  server;
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
 	server.sin_addr.s_addr = INADDR_ANY;
-	bzero(server.sin_zero, 8); /*Advise for standar posix */
-	int r = bind(server_code, (struct sockaddr *)&server, size);
-	if (r == ERROR){
-		cerr<<"SERVER :: Error in binding\n";
-        return;
-	}
-	r = listen(server_code, BACKLOG);
-	if (r == ERROR){
-		cerr << "SERVER :: Error in listening\n";
-        return;
-	}
+	bzero(server.sin_zero, 8);
 
-	//Now the client
-	int client_code;
-	struct sockaddr_in  client;
-	size_t size_cli;
-	cout << "\n\t\tServer: Waiting for a client..." << endl;
-	int cont = 0;
-	int result;
-	int arg =1,c; /*  c -> control variable */
-
-	while(client_code = accept(server_code, (struct sockaddr *)& client, (socklen_t*)&size_cli)){
-
-		int *int_code = new int[0];
-		int_code[0] = client_code;
+    /* Binding */
+	res = bind(server_code, (struct sockaddr *)&server, sizeof(sockaddr));
+	if (res == ERROR){
+        perror("SERVER :: Error in binding\n");
+        close(server_code);
+    }
+    /* Listening */
+	res = listen(server_code, BACKLOG);
+	if (res == ERROR) perror("SERVER :: Error in listening\n");
 
 
+	printf("\n SERVER:: Waiting for a client...\n");
+
+	while( cont_client < NUM_CLIENTS){
+        //Capturing the code of the client
+        client_code = accept(server_code, (struct sockaddr *)& client, (socklen_t*)&size_cli);
+
+        if(client_code < 0) {
+            close(client_code);
+            continue;
+        }
+
+        //Save the ip of the client of a map
         clients_map[client_code] = inet_ntoa(client.sin_addr);
-        cout << "\t\t The client " << clients_map[client_code] << " : Has connected\n";
+        cout << " The client " << clients_map[client_code] << " : Has connected\n";
 
-		if (r == ERROR){
-			cerr << "SERVER :: Error accepting";
-		}
-		if(pthread_create(&hilo[cont], NULL,  function, (void*) int_code)!= 0){
+		if (res == ERROR) cerr << "SERVER :: Error accepting";
+
+		if(pthread_create(&hilo[cont_client++], NULL,  hold_client, (void*) &client_code)!= 0){
 			perror("There was an error creating the thread.");
 		}
 	}
@@ -152,26 +173,12 @@ void create_server(){
 	close(server_code);
 }
 
-void load_data(bool from_scratch){
-    if(!from_scratch){
-        cout << "\t Server: The data has  been read from disk" <<endl;
-        data = read_file_hash(); //hash_table
-        ll total = 0;
-        for(int i=0; i< size_hash_table; i++){
-            total += data[i].size();
-        }
-        LOC = total; //re pos end of the file for insertion
-        cout << "\t Server: The number or registers that were readen from disk were: " << (total -1)<< endl;
-    }else{
-        create_random_data();
-        write_hash_table();
-    }
-}
+
 
 int main(){
-    cout <<" \n ADMIN : REMEMBER THAT THE FILE var/structures.bin must exist. \n";
-    bool aux = false;
-    load_data(aux);
+    printf("\nADMIN : REMEMBER THAT THE FILE var/structures.bin must exist. \n");
+    bool load_data_from_scratch = false;
+    load_data(load_data_from_scratch );
     create_server();
 	return 0;
 }
